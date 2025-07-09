4755ed2ed19aac0712fed50cb796face9e743272
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-07-09 17:53:43 -0300

    feat: implement theme service for dark mode support
    - Added ThemeService to manage dark mode functionality.
    - Integrated theme toggling in the menu component.
    - Updated menu styles to reflect theme changes.
    - Configured Tailwind CSS for dark mode support.
    - Added autoprefixer to PostCSS configuration.
    - Updated package.json to include necessary dependencies.
    - Created unit tests for ThemeService.

--- src/app/pages/layout/login/login.ts
+++ src/app/pages/layout/login/login.ts
@@ -1,10 +1,12 @@
-import { Component, EventEmitter, inject } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 import { HlmButtonDirective } from '@spartan-ng/helm/button';
 import { AuthService } from '../../../auth/auth.service';
 import Swal from 'sweetalert2'
 import { InputCustom } from "../../../components/input-custom/input-custom";
+import { HlmSpinnerComponent } from '@spartan-ng/helm/spinner';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -12,43 +14,45 @@ import { InputCustom } from "../../../components/input-custom/input-custom";
   imports: [
     RouterLink,
     HlmButtonDirective,
-    InputCustom
-],
+    InputCustom,
+    HlmSpinnerComponent,
+    CommonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  public objeto = { login: '',  senha: '', };
-  router = inject(Router);
-  constructor(private auth: AuthService) {}
+  public objeto = { login: '', senha: '', };
+  public loading = false; 
 
-  // To:do -  colocar processamento enquanto o servidor nao responde:
+  router = inject(Router);
+  constructor(private auth: AuthService) { }
 
   logar() {
+    this.loading = true;
     this.auth.login(this.objeto).subscribe({
       next: (res: any) => {
+         this.loading = false; 
         this.auth.setUser(res)
         this.auth.setToken(res.Authorization);
         this.router.navigate(['admin/home']);
       },
       error: (err) => {
+          this.loading = false;
         this.tratarErro(err);
       },
     });
   }
 
-  tratarErro(e: any)
-  {
+  tratarErro(e: any) {
 
     const status = e.status ?? null;
     let mensagem = "Login ou senha incorreto"
 
-    if(status == 403)
-    {
-       mensagem = 'Escopo(s) inválido(s) fornecido(s)';
+    if (status == 403) {
+      mensagem = 'Escopo(s) inválido(s) fornecido(s)';
     }
-    else if (status == 0)
-    {
+    else if (status == 0) {
       mensagem = 'Sem comunicação com o servidor.';
     }
 
