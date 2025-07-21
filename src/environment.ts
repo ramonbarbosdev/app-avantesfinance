e4ebd1649b76e544367222343392aca77249b762
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-07-21 17:40:42 -0300

    feat: cors

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/avantesfinance',
-//   apiUrl: 'https://api-chat-socket.onrender.com/chatsocket',
+  // apiUrl: 'http://localhost:8080/avantesfinance',
+  apiUrl: 'https://apiavantes.ramoncode.com.br/avantesfinance',
   // apiUrlWebSocket: '//localhost:8080/chatsocket/chat-socket',
-//   apiUrlWebSocket:
-    // 'https://api-chat-socket.onrender.com/chatsocket/chat-socket',
+  //   apiUrlWebSocket:
+  // 'https://api-chat-socket.onrender.com/chatsocket/chat-socket',
 };
